fce8a547c5629b5eac64962c3fe31fa4b08f669f
Memerlin <lucy_lerma@hotmail.es>
2023-09-28 19:06:03 -0600

    Run on cuda if available

--- preprocess_and_training.py
+++ preprocess_and_training.py
@@ -19,7 +19,7 @@ from Convert_csv_to_JSONl import calculate_max_sentence_length
 from torch.utils.data import DataLoader
 
 
-device = torch.device("cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 data = pd.read_json('training-data2.jsonl', lines=True)
 #Suffling dataset
